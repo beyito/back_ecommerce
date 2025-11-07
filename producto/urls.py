47eaea8af9a6e6fa1fac96555c3b8dcffd0d80ff
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-11-07 12:42:18 -0400

    CRUD DE CATEGORIA COMPLETADO

--- producto/urls.py
+++ producto/urls.py
@@ -1,9 +1,13 @@
 # urls.py
 from django.urls import path
 # from rest_framework_simplejwt.views import TokenRefreshView
-
+from . import views
 urlpatterns = [
-    # path('listar_activas', listar_categorias_activas, name='listar_categorias_activas'),
-    # path('categorias/todas/', listar_categorias, name='listar_categorias'),
-    # path('categorias/<int:categoria_id>/', obtener_categoria_por_id, name='obtener_categoria_por_id'),
+    path('crear_categoria', views.crear_categoria, name='crear_categoria'),
+    path('editar_categoria/<int:categoria_id>', views.editar_categoria, name='editar_categoria'),
+    path('eliminar_categoria/<int:categoria_id>', views.eliminar_categoria, name='eliminar_categoria'),
+    path('activar_categoria/<int:categoria_id>', views.activar_categoria, name='activar_categoria'),
+    path('listar_activas', views.listar_categorias_activas, name='listar_categorias_activas'),
+    path('listar_categorias', views.listar_categorias, name='listar_categorias'),
+    path('obtener_categoria/<int:categoria_id>/', views.obtener_categoria_por_id, name='obtener_categoria_por_id'),
 ]
